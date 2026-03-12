d8a7b0bc8090ab434674acf1bc586ed06b249f3b
Gaby Nieva <gaby.nieva@outlook.com>
2026-03-12 11:04:12 -0600

    Agregando miniaturas

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ const data = [
   { id: "p06", title: "Ruta", desc: "Camino en perspectiva", src: "https://picsum.photos/id/1005/1200/675" }
 ];
 
+// Recuperar elementos del DOM
+const thumbs = document.querySelector("#thumbs"); //miniaturas
+const heroImg = document.querySelector("#heroImg");  // imagen principal
+const heroTitle = document.querySelector("#heroTitle"); // título de la imagen
+const heroDesc = document.querySelector("#heroDesc"); // descripción de la imagen
+const counter = document.querySelector("#counter"); // contador de imágenes
+const likeBtn = document.querySelector("#likeBtn"); // botón de "me gusta"
+
+// Trabajar con el estado de la aplicación
+let currentIndex = 0; // Índice de la imagen actual
+const likes ={}; // Objeto para almacenar los "me gusta" por imagen
+
+// Renderizar las miniaturas
+function renderThumbs() {
+  thumbs.innerHTML = data.map((item, index) => {
+    return `
+      <article class="thumb ${index === currentIndex ? "active" : ""}" data-index="${index}">
+        <span class="badge">${index + 1}</span>
+        <img src="${item.src}" alt="${item.title}" />
+      </article>
+    `;
+   }).join("");
+}
+
+renderThumbs(); // Llamar a la función para mostrar las miniaturas
